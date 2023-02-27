39a21a151960a5cf362ad6acc0036f4d96b779fe
Noan07 <84435602+Noan07@users.noreply.github.com>
2023-02-27 16:47:51 +0100

    :hammer: Ajout dockerFile + Ajout tests

---
+++ code/server/TestControleurs/UnitTestGames.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestControleurs
+{
+    [TestClass]
+    public class UnitTestGames
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
