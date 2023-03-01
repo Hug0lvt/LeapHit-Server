0393fd9c9a6bb69b9d67dccbd49f30f652405b97
Noan RANDON <noan.randon@etu.uca.fr>
2023-03-01 12:25:43 +0100

    :merge: résolution conflits

--- code/server/TestControleurs/UnitTestGames.cs
+++ code/server/TestControleurs/UnitTestGames.cs
@@ -1,5 +1,6 @@
 using ApiLeapHit.Controllers;
 using ApiLeapHit.Mapper;
+using DataBase.Context;
 using DataBase.DataManager;
 using DataBase.Entity;
 using DTO;
@@ -7,8 +8,6 @@ using DTO.Factory;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json.Linq;
-using System;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -22,23 +21,26 @@ namespace TestControleurs
         public async Task TestGetPlayer_ValidId()
         {
             // Arrange
-            int id = 1;
+            int id = 8;
             DbDataManager dataManager = new DbDataManager();
             ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
             var player = new Player { playerId = id, name = "Test Player", nbBallTouchTotal = 0, timePlayed = 3 };
             var controller = new PlayerController(dataManager, loggerFactory.CreateLogger<PlayerController>());
-            var rep= await controller.AddPlayer(player.ToDto());
-            Console.WriteLine(rep.ToString());
+
+
+            var rep = await controller.AddPlayer(player.ToDto());
+
             // Act
             var result = await controller.GetPlayer(id);
-            var objectResult = (ObjectResult)(result.Result);
-            var apiResponse = result.Value;
+            var objectResult = (ObjectResult)result.Result;
+            var apiResponse = (ApiResponse<DTOPlayer>)objectResult.Value;
 
             // Assert
-            Assert.IsNotNull(apiResponse);
-
+            Assert.IsNotNull(objectResult);
             Assert.AreEqual((int)HttpStatusCode.OK, objectResult.StatusCode);
-            Assert.AreEqual(apiResponse.playerId, id);
+            Assert.AreEqual(apiResponse.Data.playerId, id);
+         
         }
+
     }
 }
