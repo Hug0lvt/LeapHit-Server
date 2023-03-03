ac074ed3d92ddc068f3ca1ed43fcc8d65a2855f0
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-03-03 11:51:01 +0100

    :hammer: Des que le serveur reçoi deux données ça doit lancer le match.

--- code/server/Server/Program.cs
+++ code/server/Server/Program.cs
@@ -5,6 +5,8 @@ using System.Net.Sockets;
 
 class Program
 {
+
+    static int playerCount = 0;
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to LeapHit Multiplayer - Server");
@@ -18,12 +20,22 @@ class Program
         Console.WriteLine("Server started, waiting for clients to connect...");
         Console.WriteLine(endPoint.Address.ToString());
 
-        while (true)
+        while (playerCount < 2)
         {
             IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
             byte[] data = server.Receive(ref clientEndPoint);
             string dataReceived = System.Text.Encoding.ASCII.GetString(data);
             Console.WriteLine("Data received from client: " + dataReceived + " from " + clientEndPoint.ToString());
+         
+            playerCount++;
+
+            if (playerCount == 2)
+            {
+                Console.WriteLine("Deux joueurs connectés, le jeu va commencer...");
+                // On va mettre le code du pour demarrer le match ici
+            }
+
         }
+        server.Close();
     }
 }
