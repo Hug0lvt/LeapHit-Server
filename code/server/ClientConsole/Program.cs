4c7ff23ca954520d595c5dc57872aa7b000748b0
Hugo LIVET <Hugo.LIVET@etu.uca.fr>
2023-03-07 20:38:59 +0100

    add clientconsole (ceated by Bruno)

---
+++ code/server/ClientConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Pong Multiplayer - Client");
+        StartClient();
+    }
+
+    static void StartClient()
+    {
+        //IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("82.165.19.245"), 3131);
+        IPEndPoint endPoint = new IPEndPoint(Dns.GetHostAddresses("leaphit.hulivet.fr").FirstOrDefault(), 3131);
+        UdpClient client = new UdpClient();
+        Console.WriteLine("Client connected to server: " + endPoint.ToString());
+
+        while (true)
+        {
+            Console.WriteLine("Enter data to send to server:");
+            string data = Console.ReadLine();
+            byte[] dataToSend = System.Text.Encoding.ASCII.GetBytes(data);
+            client.Send(dataToSend, dataToSend.Length, endPoint);
+        }
+    }
+}
