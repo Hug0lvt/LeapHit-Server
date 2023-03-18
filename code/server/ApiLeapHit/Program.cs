56a8c35eb3734315849a70b84cdd126919049c3f
Noan07 <84435602+Noan07@users.noreply.github.com>
2023-03-18 07:29:22 +0100

    :hammer: modif Program.cs de l'Api

--- code/server/ApiLeapHit/Program.cs
+++ code/server/ApiLeapHit/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<DbDataManager>();
+builder.Services.AddSingleton<DbDataManager>();
 //builder.Services.AddSingleton<IDataManager, StubData>();
 
 // Add logging
